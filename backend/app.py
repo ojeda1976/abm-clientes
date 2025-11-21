8d0512f0b8f62f3d5ce6290b8450cf4c12b0217d
ojeda1976 <Jojeda.mail@gmail.com>
2025-11-21 13:08:29 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -123,11 +123,10 @@ def borrar_cliente(id):
     return jsonify({"message": "Cliente eliminado"})
 
 # --- Bloque para arrancar el servidor ---
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()   # crea las tablas si no existen
 import os
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()   # crea las tablas si no existen
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
