dbb6739eecd33e43947f5830efea293d96d1456f
ojeda1976 <Jojeda.mail@gmail.com>
2025-11-21 13:05:02 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -126,4 +126,8 @@ def borrar_cliente(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()   # crea las tablas si no existen
-    app.run(debug=True, port=4000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
